b56fb1383a8c63e2117c3dc7ba32379ccb6ba4b5
nimeshlad <nimesh.kumar@digitalindia.gov.in>
2025-02-26 22:52:59 +0530

    Registration Login Verify user by email

--- pcart/settings.py
+++ pcart/settings.py
@@ -141,3 +141,17 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
+
+# SMTP Configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'your-email@gmail.com'
+EMAIL_HOST_PASSWORD = 'your-app-password'
+EMAIL_USE_TLS = True
+
